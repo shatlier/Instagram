429c9ee902cf124af830fc690b75235aa4f9d916
Akif Hameed <Akif Hameed>
2017-02-27 08:52:56 +0500

    Initial commit

---
+++ app/src/main/java/com/akif/instagramandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.akif.instagramandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
